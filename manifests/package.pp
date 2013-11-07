9f9b71b0886675a006f736ccd9d606d5e9b3e328
Jon Evans <jon@springyweb.com>
2013-11-07 16:10:54 +0000

    Revert "Add ability to install on RHEL5"
    This reverts commit c5f545882eaab3bd6c6744221fac60593860396a.

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,30 +4,7 @@
 #
 # The package might not specify a dependency on Java, so you may need to
 # specify that yourself
-class jenkins::package(
-  $version = 'installed',
-  $repo = undef
-) {
-
-  $yum_jenkins_gpg = $::lsbmajdistrelease ? {
-      '5'     => '/etc/pki/rpm-gpg/RPM-GPG-KEY-jenkins.el5',
-      default => '/etc/pki/rpm-gpg/RPM-GPG-KEY-jenkins',
-  }
-  
-  file { $yum_jenkins_gpg :
-      ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => "puppet:///modules/jenkins/${yum_jenkins_gpg}";
-  }
-  
-  exec { 'httpd_yum_install':
-      command => "/usr/bin/yum --disablerepo=* --enablerepo=${repo} -y install jenkins",
-      creates => '/usr/lib/jenkins/jenkins.war',
-      require => File[$yum_jenkins_gpg],
-  }
-  
+class jenkins::package($version = 'installed') {
   package {
     'jenkins' :
       ensure => $version;
